d7884fd4cc36e29f5d15a55813245a7e3c1dd506
ur2ku <lim8667@gmail.com>
2022-04-29 11:50:22 +0900

    🎆

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import {Text} from "react-native";
 import {ScrollView} from "react-native";
 
 const {width:SCREEN_WIDTH} = Dimensions.get("window");
-const API_KEY = "358b41330582446b0266246023c6eacb";
 
+const API_KEY = "358b41330582446b0266246023c6eacb";
 export default function App() {
+
+    const [days,setDays] = useState("loading..");
     const [city,setCity] = useState("loading..");
     const [ok, setOk] = useState(true);
     // 권한요청 (앱 사용중에만 위치를 요청)
@@ -18,9 +20,9 @@ export default function App() {
             coords: {latitude, longitude},
         } = await Location.getCurrentPositionAsync({accuracy:5});
 
-       /* const {
-            latitude, longitude
-        } = await (await Location.getCurrentPositionAsync({accuracy: 5})).coords;*/
+        /* const {
+             latitude, longitude
+         } = await (await Location.getCurrentPositionAsync({accuracy: 5})).coords;*/
 
         if(!granted){
             setOk(false);
@@ -32,6 +34,11 @@ export default function App() {
         );
         console.log(location[0].region);
         setCity(location[0].region);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=${API_KEY}&unites=metric`);
+        const json = await response.json();
+        const ete = json.mappings;
+        console.log(response.json.getAttributeNames()[3]);
+
     };
 
     // 컴포넌트가 마운트 되면 useEffect 를 사용해서 getPermission 함수를 호출
@@ -51,9 +58,8 @@ export default function App() {
                 contentContainerStyle={styles.weather}
                 style={{backgroundColor: 'white'}}
             >
-
                 <View style={styles.day}>
-                    <Text style={styles.temp}>27</Text>
+                    <Text style={styles.temp}>{/*{parseFloat(days.temp.days).toFixed(1)}*/}</Text>
                     <Text style={styles.description}>sunny</Text>
                 </View>
 
@@ -71,12 +77,11 @@ export default function App() {
                     <Text style={styles.temp}>27</Text>
                     <Text style={styles.description}>sunny</Text>
                 </View>
-
             </ScrollView>
 
         </View>
-    )
-};
+    );
+}
 
 const styles = StyleSheet.create({
     container:{
